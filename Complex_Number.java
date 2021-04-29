58d75388aa7d40e681ac3a317a33a13de8ffeb0e
Shreevatsa3942 <sbalthillaya584@gmail.com>
2021-04-29 23:37:08 +0530

    29-04-2021-Complex_number

---
+++ Complex_Number.java
@@ -0,0 +1,61 @@
+/*Create a class ADD_NUM with two instance members num1,num2.
+Create subclass ADD_REAL and ADD_COMPLEX and a method to Add() num1 and num2
+using dynamic method dispatch concept.*/
+
+//Name:Shreevatsa
+//Reg no:200970093
+package javaprogramming;
+
+import java.util.Scanner;
+
+class ADD_NUM{
+    int num1,num2;
+
+    ADD_NUM(){
+        num1=num2=0;
+    }
+    int Add(){
+        return 0;
+    };
+}
+class ADD_REAL extends ADD_NUM{
+
+    ADD_REAL(int num1,int num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+    //method overriding
+    public int Add(){
+        return (num1+num2);
+    }
+}
+class ADD_COMPLEX extends ADD_NUM{
+
+    ADD_COMPLEX(int num1,int num2){
+        this.num1=num1;
+        this.num2=num2;
+    }
+    //method overriding
+    public int Add(){
+        return (num1+num2);
+    }
+}
+public class Complex_Number {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\nEnter two Real Numbers: ");
+        System.out.print("\n Num1 : ");int r1=sc.nextInt();
+        System.out.print("\n Num2 : ");int r2= sc.nextInt();
+        ADD_NUM obj1=new ADD_REAL(r1,r2);
+        System.out.println("\nEnter two Complex Numbers: ");
+        System.out.print("\n Num1 : ");int c1=sc.nextInt();
+        System.out.print("\n Num2 : ");int c2= sc.nextInt();
+        ADD_NUM obj2=new ADD_COMPLEX(c1,c2);
+
+        int result= obj1.Add();
+        System.out.println("\nSum of two Real Number: "+result);
+
+        result=obj2.Add();
+        System.out.println("\nSum of two Imaginary Number: "+result);
+    }
+}
